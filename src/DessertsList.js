6747ba016475f027a856b3049402d441952dce90
Hannah.Celemen <hcelemen@icloud.com>
2024-02-13 12:44:02 -0700

    Define DessertsList

--- src/DessertsList.js
+++ src/DessertsList.js
@@ -1,6 +1,14 @@
 function DessertsList(props) {
-  // Implement the component here.
-  return null;
+  const desserts = props.data
+    .filter((item) => item.calories < 500)
+    .sort((a, b) => a.calories - b.calories)
+    .map((item, index) => (
+      <li key={index}>
+        {item.name} - {item.calories} cal
+      </li>
+    ));
+  console.log(desserts);
+  return <ul>{desserts}</ul>;
 }
 
 export default DessertsList;
