1f32ed87d908ec2cfd67d387c8077a0de9f0615c
Hannah.Celemen <hcelemen@icloud.com>
2024-02-13 16:47:31 -0700

    Add Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import DessertsList from "./DessertsList";
+import Form from "./Form";
 
 const desserts = [
   {
@@ -29,6 +30,7 @@ function App() {
     <div className="App">
       <h2>List of low calorie desserts:</h2>
       <DessertsList data={desserts} />
+      <Form />
     </div>
   );
 }
